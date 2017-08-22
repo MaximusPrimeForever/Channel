12385aedc2ddfe3b4200b441d221938d6062e100
Max <max.anisimov230@gmail.com>
2017-08-22 21:30:33 +0300

    Switched back to TCP. Two Channel processes can pass messages now.

--- channel.py
+++ channel.py
@@ -3,7 +3,8 @@ import socket
 print("Channel ver." + "1.0")
 print("Listen or Connect?")
 usr_inp = 0
-port = 5984
+m_port = 5984
+m_input_str = "> "
 
 while usr_inp == 0:
     try:
@@ -11,33 +12,37 @@ while usr_inp == 0:
     except ValueError:
         print("Bad input")
 
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if usr_inp == 1:
-    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host = socket.gethostname()
-    conn.bind((host, port))
+    # listen
+    conn.bind(('', m_port))
+    conn.listen(1)
 
+    conn, addr = conn.accept()
+    print('Connection address:', addr)
     while True:
         recv_msg = conn.recv(1024)
         if not recv_msg:
             break
 
-        print(recv_msg)
-        msg = input("> ")
-        conn.sendto(msg)
+        print(recv_msg.decode())
+        msg = input(m_input_str)
+        conn.send(msg.encode())
     conn.close()
 
 else:
-    # peer = input("Peer IP: ")
-    peer = socket.gethostname()
-    conn = socket.socket()
-    conn.connect((peer, port))
+    # connect
+    peer = input("Peer IP: ")
+    msg = input(m_input_str)
 
+    conn.connect((peer, m_port))
+    conn.send(msg.encode())
     while True:
         recv_msg = conn.recv(1024)
         if not recv_msg:
             break
 
-        print(recv_msg)
-        msg = input("> ")
-        conn.sendto(msg)
+        print(recv_msg.decode())
+        msg = input(m_input_str)
+        conn.send(msg.encode())
     conn.close()
